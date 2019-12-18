b21729ea59246c12e0494f971013f8a3fe616163
Joss <34286057+JosephMasaka@users.noreply.github.com>
2019-12-18 17:03:52 +0300

    Project update
    updated my project and added the project files

---
+++ README.md
@@ -0,0 +1,9 @@
+# Answer Message Board
+
+This README outlines the details of th app and what it does
+
+## Author
+
+This app was developed by
+
+* Cliff Gor
---
+++ config/environment.js
@@ -0,0 +1,58 @@
+/* eslint-env node */
+
+module.exports = function(environment) {
+  var ENV = {
+    modulePrefix: 'super-rentals',
+    environment: environment,
+    rootURL: '/',
+    locationType: 'auto',
+    EmberENV: {
+      FEATURES: {
+        // Here you can enable experimental features on an ember canary build
+        // e.g. 'with-controller': true
+      },
+      EXTEND_PROTOTYPES: {
+        // Prevent Ember Data from overriding Date.parse.
+        Date: false
+      }
+    },
+
+    firebase: {
+      apiKey: "AIzaSyAX1Uvndpcj7MEsl0aozMMqY-HSVi2piLU",
+      authDomain: "super-rentals-b56ba.firebaseapp.com",
+      databaseURL: "https://super-rentals-b56ba.firebaseio.com",
+      storageBucket: "super-rentals-b56ba.appspot.com",
+      messagingSenderId: "694263626650"
+    },
+
+    APP: {
+      // Here you can pass flags/options to your application instance
+      // when it is created
+    }
+  };
+
+  if (environment === 'development') {
+    // ENV.APP.LOG_RESOLVER = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_TRANSITIONS = true;
+    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+  }
+
+  if (environment === 'test') {
+    // Testem prefers this...
+    ENV.locationType = 'none';
+
+    // keep test console output quieter
+    ENV.APP.LOG_ACTIVE_GENERATION = false;
+    ENV.APP.LOG_VIEW_LOOKUPS = false;
+
+    ENV.APP.rootElement = '#ember-testing';
+  }
+
+  if (environment === 'production') {
+
+  }
+
+  return ENV;
+};
